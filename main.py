efafeba44cffa25adcd465c67cef286445475a73
AD\mayumi.ohta <mayumi.ohta@zo.uni-heidelberg.de>
2016-08-25 17:43:49 +0200

    test

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ import predict
 import json
 
 app = flask.Flask(__name__)
-# app.debug = True
+app.debug = True
 
 
 @app.route('/predict', methods=['GET'])
 def main():
     text = flask.request.args.get('text')
     res = predict.predict(text)
+    return json.dumps(res, ensure_ascii=False, indent=2)
+
+@app.route('/test', methods=['GET'])
+def main():
+    text = flask.request.args.get('text')
+    res = {'text': text}
     return json.dumps(res, ensure_ascii=False, indent=2)
 
 
